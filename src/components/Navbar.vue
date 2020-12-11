1521daddc5a638f765e1191ccf0c2195e21c9942
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2020-12-11 16:26:41 +0700

    asss

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="light">
+            <div class="container">
+                <b-navbar-brand to="/">Kulineran</b-navbar-brand>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <router-link to="/" class="nav-link">Home</router-link>
+                        <router-link to="/foods" class="nav-link">Foods</router-link>
+                    </b-navbar-nav>
+
+                    <!-- Right aligned nav items -->
+                    <b-navbar-nav class="ml-auto">
+                        <li>
+                            <router-link class="nav-link" to="/keranjang">keranjang
+                            <b-icon-bag/>
+                            <span class="badge badge-success ml-2">{{ updateKeranjang ? updateKeranjang.length : jumlah_pesanans.length }}</span>
+                            </router-link>
+                        </li>
+                    </b-navbar-nav>
+                </b-collapse>
+            </div>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      jumlah_pesanans: [],
+    };
+  },
+  props: ["updateKeranjang"],
+  methods: {
+    setJumlah(data) {
+      this.jumlah_pesanans = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) => this.setJumlah(response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style>
+</style>
